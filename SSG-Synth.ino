94e702daaeb0c58d40cae0ffbe2392f5df1f8789
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-03-12 07:24:13 +0100

    Create Arduino project

---
+++ SSG-Synth.ino
@@ -0,0 +1,26 @@
+/* 
+ * Audio synthesizer based on the Yamaha YM2149F sound chips
+ * 
+ * Compatibility :
+ * 
+ * YM2149F (SSG)
+ * Two possible operating modes:
+ * - 3 monophonic instruments (default)
+ * - 1 polyphonic (3 voices) instrument
+ * 
+ * +--------+      +---------+      +---------+
+ * | MIDI   |      | ARDUINO |      |   SSG   |
+ * | INPUT  | ---> |   UNO   | ---> | YM2149F | ---> Sound
+ * +--------+      +---------+      +---------+
+ * 
+ */
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
